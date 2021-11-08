aa4e328690d6c616678d7c09c8d63e2885ac3f75
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2021-11-08 20:51:00 +0530

    boiler plate update

---
+++ src/components/HomeScreenComponent/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './HomeScreenComponent.module.css'
+
+const index = () => {
+    return (
+        <div className={styles['home']}>
+            <span>hello</span>
+        </div>
+    )
+}
+
+export default index
