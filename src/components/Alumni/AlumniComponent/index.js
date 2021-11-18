6b6631a31a6cde0d749655f6589acef80d3b2eac
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2021-11-19 02:36:42 +0530

    sidebar added (#15)

--- src/components/Alumni/AlumniComponent/index.js
+++ src/components/Alumni/AlumniComponent/index.js
@@ -1,72 +1,25 @@
 import React, { useState } from 'react';
 import styles from './Alumni.Module.css';
 import quotes from '../data';
-import { Link } from 'react-router-dom';
 
 const Index = () => {
+    const [tab, setTab] = useState(1);
+    const handleClick = (num) => {
+        setTab(num);
+
+    }
+    console.log(tab);
     return (
         <div className={styles['alumni-component']}>
             <span className={styles['title']}>Alumni</span>
             <span className={styles['title-underline']}></span>
             <div className={styles['years']}>
-                <Link
-                    to={{
-                        pathname: '/quotescreen',
-                        state: {
-                            quote: quotes
-                        }
-                    }}
-                    style={{ textDecoration: 'none' }}
-                    className={styles['year']}>
-                    2015
-                </Link>
-                <Link
-                    to={{
-                        pathname: '/quotescreen',
-                        state: {
-                            quote: quotes.filter(quote => quote.cat === '2016')
-                        }
-                    }}
-                    style={{ textDecoration: 'none' }}
-                    className={styles['year']}>
-                    2016
-                </Link>
-                <Link
-                    to={{
-                        pathname: '/quotescreen',
-                        state: quotes.filter(quote => quote.cat === '2017')
-                    }}
-                    style={{ textDecoration: 'none' }}
-                    className={styles['year']}>
-                    2017
-                </Link>
-                <Link
-                    to={{
-                        pathname: '/quotescreen',
-                        state: quotes.filter(quote => quote.cat === '2018')
-                    }}
-                    style={{ textDecoration: 'none' }}
-                    className={styles['year']}>
-                    2018
-                </Link>
-                <Link
-                    to={{
-                        pathname: '/quotescreen',
-                        state: quotes.filter(quote => quote.cat === '2019')
-                    }}
-                    style={{ textDecoration: 'none' }}
-                    className={styles['year']}>
-                    2019
-                </Link>
-                <Link
-                    to={{
-                        pathname: '/quotescreen',
-                        state: quotes.filter(quote => quote.cat === '2020')
-                    }}
-                    style={{ textDecoration: 'none' }}
-                    className={styles['year']}>
-                    2020
-                </Link>
+                <div className={styles['year']} onClick={() => handleClick(1)} >2015</div>
+                <div className={styles['year']} onClick={() => handleClick(2)}>2016</div>
+                <div className={styles['year']} onClick={() => handleClick(3)}>2017</div>
+                <div className={styles['year']} onClick={() => handleClick(4)}>2018</div>
+                <div className={styles['year']} onClick={() => handleClick(5)}>2019</div>
+                <div className={styles['year']} onClick={() => handleClick(6)}>2020</div>
             </div>
             <span className={styles['title']}>Alumni Achievements</span>
             <span className={styles['title-underline']}></span>
