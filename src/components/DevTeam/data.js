f2d080f44db0e5c3b46365372fd02e3c40507e0d
Diya Karmakar <diya20_ug@ece.nits.ac.in>
2021-12-06 00:02:22 +0530

    devteam updated

--- src/components/DevTeam/data.js
+++ src/components/DevTeam/data.js
@@ -1,12 +1,112 @@
 import abhishek from '../../uploads/devteam/abhishek.png';
+import rahul from '../../uploads/devteam/rahul.jpeg';
+import Agneesh from '../../uploads/devteam/Agneesh.png';
+import Abhishek_j from '../../uploads/devteam/Abhishek_j.jpg';
+import Anurag from '../../uploads/devteam/Anurag.jpg';
+import Anindita from '../../uploads/devteam/Anindita.jpg';
+import Ankit from '../../uploads/devteam/Ankit.jpeg';
+import Dharani from '../../uploads/devteam/Dharani.jpeg';
+import Dipayon from '../../uploads/devteam/Dipayon.jpeg';
+import Diya from '../../uploads/devteam/Diya.png';
+import Pritam from '../../uploads/devteam/Pritam.jpeg';
+import Reisha from '../../uploads/devteam/Reisha.jpg';
+import Ridha from '../../uploads/devteam/Ridha.jpeg';
+import Saurav from '../../uploads/devteam/Saurav.jpeg';
+import Shubham from '../../uploads/devteam/Shubham.png';
 
-const images = [
-    {
-        id: 1,
+     const images = [   
+    {    id: 1,
         src: abhishek,
         name: 'Abhishek Bharadwaz',
         year: '3rd Year'
+    },
+    {
+        id: 2,
+        src: rahul,
+        name: 'Rahul Sihag',
+        year: '3rd Year'
+    },
+    {
+        id: 3,
+        src: Abhishek_j,
+        name: 'Abhishek',
+        year: '2nd Year'
+    },
+    {
+        id: 4,
+        src: Agneesh Dasgupta,
+        name: 'Agneesh',
+        year: '2nd Year'
+    },
+    {
+        id: 5,
+        src: Anurag,
+        name: 'Anurag Debnath',
+        year: '2nd Year'
+    },
+    {
+        id: 6,
+        src: Anindita,
+        name: 'Anindita Kakati',
+        year: '2nd Year'
+    },
+    {
+        id: 7,
+        src: Ankit,
+        name: 'Ankit Chauhan',
+        year: '2nd Year'
+    },
+    {
+        id: 8,
+        src: Dharani,
+        name: 'Dharani Pamidimukkala',
+        year: '2nd Year'
+    },
+    {
+        id: 9,
+        src: Dipayon,
+        name: 'Dipayon Ghosh',
+        year: '2nd Year'
+    },
+    {
+        id: 10,
+        src: Diya,
+        name: 'Diya Karmakar',
+        year: '2nd Year'
+    },
+    {
+        id: 11,
+        src: Pritam,
+        name: 'Pritam Biswas',
+        year: '2nd Year'
+    },
+    {
+        id: 12,
+        src: Reisha,
+        name: 'Reisha Ahmed',
+        year: '2nd Year'
+    },
+    {
+        id: 13,
+        src: Ridha,
+        name: 'Ridha Rani Pradhan',
+        year: '2nd Year'
+    },
+    {
+        id: 14,
+        src: Saurav,
+        name: 'Saurav Suman',
+        year: '2nd Year'
+    },
+    {
+        id: 15,
+        src: Shubham,
+        name: 'Shubham Kr Singh',
+        year: '2nd Year'
     }
+   
+   
+   
 ]
 
 export default images;
