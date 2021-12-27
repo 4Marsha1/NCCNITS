3417954c4316e22fac359d729b3473b60d0c2fb7
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2021-12-28 03:46:37 +0530

    back to top btn animation

--- src/components/ToTheTop/index.js
+++ src/components/ToTheTop/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './style.module.css'
 
-const index = () => {
+const Index = () => {
+
+    const [visible, setVisible] = useState(false)
+
+    const toggleVisible = () => {
+        const scrolled = document.documentElement.scrollTop;
+        if (scrolled > 300) {
+            setVisible(true)
+        } else {
+            setVisible(false)
+        }
+    };
+
+    const handleClick = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
+
+    window.addEventListener('scroll', toggleVisible);
+
     return (
         <>
             <a id='top'></a>
-            <button className={styles['to-top']}><a href='#top'>^</a></button>
+            {visible ? <button className={styles['to-top']} onClick={handleClick}>^</button>
+                : <></>
+            }
+
         </>
     )
 }
 
-export default index
+export default Index
