86d45665e5b1c77900e0f2dd0fd74ad62ceeeed7
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2021-12-27 21:52:00 +0530

    strength details

--- src/containers/HomeScreen.js
+++ src/containers/HomeScreen.js
@@ -3,14 +3,18 @@ import Navbar from '../components/Navbar';
 import HomeScreenComponent from '../components/HomeScreenComponent';
 import Footer from '../components/Footer';
 
-import recentUpdates from '../data/HomeScreen/data'
+import recentUpdates from '../data/HomeScreen/data';
+import strengthDetails from '../data/HomeScreen/strength';
 
 class HomeScreen extends React.Component {
     render() {
         return (
             <div>
                 <Navbar />
-                <HomeScreenComponent recentUpdates={recentUpdates} />
+                <HomeScreenComponent
+                    recentUpdates={recentUpdates}
+                    strengthDetails={strengthDetails}
+                />
                 <Footer />
             </div>
         );
---
+++ src/data/HomeScreen/strength.js
@@ -0,0 +1,20 @@
+const strengths = [
+    {
+        id: 1,
+        section: 'SD 3 Assam Battalion',
+        fourth: 31,
+        third: 40,
+        second: 33,
+        first: 0,
+    },
+    {
+        id: 2,
+        section: 'SW 62 Assam Girls Battalion',
+        fourth: 18,
+        third: 19,
+        second: 19,
+        first: 0,
+    }
+]
+
+export default strengths;
