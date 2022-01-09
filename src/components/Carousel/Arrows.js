776d66868be1a4dc68c14f40892d1b70bad4f119
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-01-09 17:56:08 +0530

    minor additions

--- src/components/Carousel/Arrows.js
+++ src/components/Carousel/Arrows.js
@@ -1,16 +1,12 @@
 import React from "react";
 
 function Arrows({ prevSlide, nextSlide }) {
-  return (
-    <div className="arrows" >
-      <span className="prev" onClick={prevSlide}>
-        &#10094;
-      </span>
-      <span className="next" onClick={nextSlide}>
-        &#10095;
-      </span>
-    </div>
-  );
+    return (
+        <div className="arrows" >
+            <button className="prev" onClick={prevSlide}><i className="fas fa-chevron-left"></i></button>
+            <button className="next" onClick={nextSlide}><i className="fas fa-chevron-right"></i></button>
+        </div>
+    );
 }
 
 export default Arrows;
