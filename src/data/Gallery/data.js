2fa755f0b070ab6e17b797e3d62456de7bf56c2f
Abhishek Bharadwaz <abhishekbharadwaz22@gmail.com>
2022-01-10 11:41:48 +0530

    photos added to gallery (#38)
    Co-authored-by: Abhishek <akg02052002@gmail.com>

--- src/data/Gallery/data.js
+++ src/data/Gallery/data.js
@@ -1,10 +1,263 @@
-const images = [
-    // template
+import image1 from '../../uploads/gallery/1.jpeg';
+import image2 from '../../uploads/gallery/2.jpeg';
+import image3 from '../../uploads/gallery/3.jpeg';
+import image4 from '../../uploads/gallery/4.jpeg';
+import image5 from '../../uploads/gallery/5.jpeg';
+import image6 from '../../uploads/gallery/6.jpeg';
+import image7 from '../../uploads/gallery/7.jpeg';
+import image8 from '../../uploads/gallery/8.jpeg';
+import image9 from '../../uploads/gallery/9.jpeg';
+import image10 from '../../uploads/gallery/10.jpeg';
+import image11 from '../../uploads/gallery/11.jpeg';
+import image12 from '../../uploads/gallery/12.jpeg';
+import image13 from '../../uploads/gallery/13.jpeg';
+import image14 from '../../uploads/gallery/14.jpeg';
+import image15 from '../../uploads/gallery/15.jpeg';
+import image16 from '../../uploads/gallery/16.jpeg';
+import image17 from '../../uploads/gallery/17.jpeg';
+import image18 from '../../uploads/gallery/18.jpeg';
+import image19 from '../../uploads/gallery/19.jpeg';
+import image20 from '../../uploads/gallery/20.jpeg';
+import image21 from '../../uploads/gallery/21.jpeg';
+import image22 from '../../uploads/gallery/22.jpeg';
+import image23 from '../../uploads/gallery/23.jpeg';
+import image24 from '../../uploads/gallery/24.jpeg';
+import image25 from '../../uploads/gallery/25.jpeg';
+import image26 from '../../uploads/gallery/26.jpeg';
+import image27 from '../../uploads/gallery/27.jpeg';
+import image28 from '../../uploads/gallery/28.jpeg';
+import image29 from '../../uploads/gallery/29.jpeg';
+import image30 from '../../uploads/gallery/30.jpeg';
+import image31 from '../../uploads/gallery/31.jpeg';
+import image32 from '../../uploads/gallery/32.jpeg';
+import image33 from '../../uploads/gallery/33.jpeg';
+import image34 from '../../uploads/gallery/34.jpeg';
+import image35 from '../../uploads/gallery/35.jpeg';
+import image36 from '../../uploads/gallery/36.jpeg';
+import image37 from '../../uploads/gallery/37.jpeg';
+import image38 from '../../uploads/gallery/38.jpeg';
+import image39 from '../../uploads/gallery/39.jpeg';
+import image40 from '../../uploads/gallery/40.jpeg';
+import image41 from '../../uploads/gallery/41.jpeg';
+import image42 from '../../uploads/gallery/42.jpeg';
+import image43 from '../../uploads/gallery/43.jpeg';
+import image44 from '../../uploads/gallery/44.jpeg';
+import image45 from '../../uploads/gallery/45.jpeg';
 
-    // {
-    //     id: 1,
-    //     url: 'https:/picsum.photos/200'
-    // },
+
+
+
+const images = [
+    {
+        id: 1,
+        url: image1
+    },
+
+    {
+        id: 2,
+        url: image2
+    },
+    
+    {
+        id: 3,
+        url: image3
+    },
+    
+    {
+        id: 4,
+        url: image4
+    },
+    
+    {
+        id: 5,
+        url: image5
+    },
+    
+    {
+        id: 6,
+        url: image6
+    },
+    
+    {
+        id: 7,
+        url: image8
+    },
+    
+    {
+        id: 9,
+        url: image9
+    },
+    
+    {
+        id: 10,
+        url: image10
+    },
+    
+    {
+        id: 11,
+        url: image11
+    },
+    
+    {
+        id: 12,
+        url: image12
+    },
+    
+    {
+        id: 13,
+        url: image13
+    },
+    
+    {
+        id: 14,
+        url: image14
+    },
+    
+    {
+        id: 15,
+        url: image15
+    },
+    
+    {
+        id: 16,
+        url: image16
+    },
+    
+    {
+        id: 17,
+        url: image17
+    },
+    
+    {
+        id: 18,
+        url: image18
+    },
+    
+    {
+        id: 19,
+        url: image19
+    },
+    
+    {
+        id: 20,
+        url: image20
+    },
+    
+    {
+        id: 21,
+        url: image21
+    },
+    
+    {
+        id: 22,
+        url: image22
+    },
+    
+    {
+        id: 23,
+        url: image23
+    },
+    
+    {
+        id: 24,
+        url: image24
+    },
+    
+    {
+        id: 25,
+        url: image25
+    },
+    
+    {
+        id: 26,
+        url: image26
+    },
+    
+    {
+        id: 27,
+        url: image27
+    },
+    
+    {
+        id: 28,
+        url: image28
+    },
+    
+    {
+        id: 29,
+        url: image29
+    },
+    
+    {
+        id: 30,
+        url: image30
+    },
+    
+    {
+        id: 31,
+        url: image31
+    },
+    
+    {
+        id: 32,
+        url: image32
+    },
+    
+    {
+        id: 33,
+        url: image33
+    },
+    
+    {
+        id: 34,
+        url: image34
+    },
+    {
+        id: 35,
+        url: image35
+    },
+    {
+        id: 36,
+        url: image36
+    },
+    {
+        id: 37,
+        url: image37
+    },
+    {
+        id: 38,
+        url: image38
+    },
+    {
+        id: 39,
+        url: image39
+    },
+    {
+        id: 40,
+        url: image40
+    },
+    {
+        id: 41,
+        url: image41
+    },
+    {
+        id: 42,
+        url: image42
+    },
+    {
+        id: 43,
+        url: image43
+    },
+    {
+        id: 44,
+        url: image44
+    },
+    {
+        id: 45,
+        url: image45
+    },
+    
+   
 ];
 
 export default images;
